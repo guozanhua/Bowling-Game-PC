e13b8a0243a797f7ec82b5da7f225fe8913256e1
malcolm maima <malcolmmaima@gmail.com>
2016-02-23 17:55:07 +0300

    Source Code

---
+++ Assets/Scripts/LaunchClick.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaunchClick : MonoBehaviour {
+
+    public float LaunchForce = 1000;
+
+    void OnMouseDown() {
+
+        rigidbody.AddForce(LaunchForce*transform.forward);
+
+    }
+}
